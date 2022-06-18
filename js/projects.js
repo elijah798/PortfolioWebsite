04bedeae6698e0d17cabeb20a5013fc45fa61492
Elijah Sellers <46266593+elijah798@users.noreply.github.com>
2022-06-18 12:50:49 -0500

    Update UI, Added Javascript file

---
+++ js/projects.js
@@ -0,0 +1,3 @@
+const invmanage = {"projectName":"Inventory Management Software", "description":"Description: Description: Java project that allows you to add parts and products to a datase(an observableList). The Products keep track of which parts are associated with it. They all have uniquely generated IDs.", "image":"/imgs/Portrait.png"}
+
+const 
